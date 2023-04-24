e923e332b7d4363515cd24f2a4a4fa17ea96319c
Daniele Della Cioppa <daniele.cioppa@akhter.co.uk>
2023-04-24 21:23:28 +0100

    uri

--- app/src/main/java/com/example/zip_json_editor/MainActivity.java
+++ app/src/main/java/com/example/zip_json_editor/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.zip_json_editor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView debugConsole;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        debugConsole = findViewById(R.id.dbgConsole);
+        debugConsole.setText("Debug Console");
+
+        Button browseButton = findViewById(R.id.browse_button);
+        browseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code to open file picker and select ZIP archive
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("application/zip");
+                startActivityForResult(Intent.createChooser(intent, "Select ZIP archive"), 1);
+
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            Uri uri = data.getData();
+            new DebugString("URI: " + uri.toString(), debugConsole);
+            // Code to handle the selected ZIP archive
+        }
+    }
+
 }
